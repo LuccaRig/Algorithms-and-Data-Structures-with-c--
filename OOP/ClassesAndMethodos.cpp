e18e6334253242b45f9c45c6659fc021016b4092
LuccaRig <137623677+LuccaRig@users.noreply.github.com>
2023-07-18 15:14:11 -0300

    Encapsulation

--- OOP/ClassesAndMethodos.cpp
+++ OOP/ClassesAndMethodos.cpp
@@ -5,27 +5,40 @@ using std::string;
 class Planet{
 //To acess the class proprieties from a function the proprieties need to be public
 public:
-    string name;
+    string Name;
     string SolarSystem;
-    int radius;
+    int Radius;
 
 //Class Methods are functions that creates the behavior of some class
     void PlanetCharacteristics(){
-        std::cout << "Name: " << name << std::endl;
+        std::cout << "Name: " << Name << std::endl;
         std::cout << "Solar System Name: " << SolarSystem << std::endl;
-        std::cout << "Radius: "<< radius << std::endl;
+        std::cout << "Radius: "<< Radius << std::endl;
+    }
+    //If you want to create a specified construct method or inicialization you need to create a function like this:
+    Planet(string name, string solar_system, int radius){
+        Name = name;
+        SolarSystem = solar_system;
+        Radius = radius;
     }
 };
 
 int main(){
     //Variable inicialization like: int A;
+    
+    /* Basic Construct inicialization:
     Planet planeta1;
-    planeta1.name = "Saturno";
+    planeta1.Name = "Saturno";
     planeta1.SolarSystem = "Sistema Solar";
-    planeta1.radius = 582326;
+    planeta1.Radius = 582326;*/
+
+    //Especified constructor inicialization:
+    Planet planeta1 = Planet("Saturno", "Sistema Solar", 582326);
+    Planet planeta2 = Planet("Netuno", "Sistema Solar", 24000);
 
     //Calling the method
     planeta1.PlanetCharacteristics();
+    planeta2.PlanetCharacteristics();
 
     
     return 0;
---
+++ OOP/Encapsulation.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using std::string;
+
+//The Functionality of Encapspsulation is to stitch together data and methods that operetes on that data
+//With that you can make sure that anything can direct acess and modify the data
+
+class Planets{
+//Every class in c++ is private by default, but is good to specify
+//Private means that no one can acess this proprieties outside the class
+private:
+    string Name;
+    string SolarSystem;
+    int Radius;
+public:
+    
+    /* Tredicional constructors are inacessible here, so are necessary getters and setters to acess the data
+        void PlanetCaracteristics(){
+        std::cout << Name << std::endl;
+        std::cout << SolarSystem << std::endl;
+        std::cout << Radius << std::endl;
+    }
+    Planets(string name, string solar_system, int radius){
+        Name = name;
+        SolarSystem = solar_system;
+        Radius = radius;
+    }*/
+    
+    void setName(string name){
+        Name = name;
+    }
+    string getName(){
+        return Name;
+    }
+    void setSolarsystem(string solar_system){
+        SolarSystem = solar_system;
+    }
+    string getSolarSystem(){
+        return SolarSystem;
+    }
+    void setRadius(int radius){
+        Radius = radius;
+    }
+    int getRadius(){
+        return Radius;
+    }
+
+};
+
+
+
+
+int main(){
+
+    /* Basic Construct inicialization is inacessible here becuse the proprieties of the class are private
+    Planets planeta1;   
+    planeta1.Name = "Saturno";
+    planeta1.SolarSystem = "Sistema Solar";
+    planeta1.Radius = 582326;
+    Thist one too:
+    Planets planeta1;
+    planeta1.PlanetCaracteristics();*/
+
+    Planets planet1;
+    planet1.setRadius(23000);
+    planet1.setName("Saturno");
+    planet1.setSolarsystem("Solar");
+    std::cout << "The Planet " << planet1.getName() << " Is on " << planet1.getSolarSystem() << ", and has: " << planet1.getRadius();  
+    
+
+    return 0;
+}
